f4ce7e9469341e1bff73ab5c95b9204ff9e87b55
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 13:30:41 +0700

    Update Fraction.cpp

--- W02/week-2/Fraction.cpp
+++ W02/week-2/Fraction.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Fraction.h"
+
+Fraction::Fraction() = default;
+
+Fraction::Fraction(int numerator, int denominator) : numerator(numerator), denominator(denominator) {}
+
+int Fraction::getNumerator() const {
+    return numerator;
+}
+
+void Fraction::setNumerator(int numerator) {
+    Fraction::numerator = numerator;
+}
+
+int Fraction::getDenominator() const {
+    return denominator;
+}
+
+void Fraction::setDenominator(int denominator) {
+    Fraction::denominator = denominator;
+}
