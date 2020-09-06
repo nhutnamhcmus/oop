8cf1e6ca109d56764f2e41f07b702142b6a6109a
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 13:35:31 +0700

    Update Rectangle.cpp

--- W02/week-2/Rectangle.cpp
+++ W02/week-2/Rectangle.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Rectangle.h"
+
+Rectangle::Rectangle(const Point &topLeft, const Point &bottomRight) : topLeft(topLeft), bottomRight(bottomRight) {}
+
+const Point &Rectangle::getTopLeft() const {
+    return topLeft;
+}
+
+void Rectangle::setTopLeft(const Point &topLeft) {
+    Rectangle::topLeft = topLeft;
+}
+
+const Point &Rectangle::getBottomRight() const {
+    return bottomRight;
+}
+
+void Rectangle::setBottomRight(const Point &bottomRight) {
+    Rectangle::bottomRight = bottomRight;
+}
