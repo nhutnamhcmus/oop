73389afec63a27db6c498b44f3582d5cc34dbf8a
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 14:04:49 +0700

    Update Circle.h

--- W03/week-3/Circle.h
+++ W03/week-3/Circle.h
@@ -4,10 +4,27 @@
 
 #ifndef WEEK_3_CIRCLE_H
 #define WEEK_3_CIRCLE_H
-
+#include <math.h>
+#include "Point.h"
 
 class Circle {
+private:
+    Point center;
+    double radius;
+public:
+    Circle(const Point &center, double radius);
 
+    [[nodiscard]] const Point &getCenter() const;
+
+    void setCenter(const Point &center);
+
+    [[nodiscard]] double getRadius() const;
+
+    void setRadius(double radius);
+
+    [[nodiscard]] double area() const;
+    [[nodiscard]] double perimeter() const;
+    [[nodiscard]] double diameter() const;
 };
 
 
