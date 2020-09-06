bdcadf336dca1060e6534e87f23d4ff261117f32
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 13:35:29 +0700

    Update Circle.h

--- W02/week-2/Circle.h
+++ W02/week-2/Circle.h
@@ -4,10 +4,22 @@
 
 #ifndef WEEK_2_CIRCLE_H
 #define WEEK_2_CIRCLE_H
-
+#include "Point.h"
 
 class Circle {
+private:
+    Point center;
+    double radius;
+public:
+    Circle(const Point &center, double radius);
 
+    [[nodiscard]] const Point &getCenter() const;
+
+    void setCenter(const Point &center);
+
+    [[nodiscard]] double getRadius() const;
+
+    void setRadius(double radius);
 };
 
 
