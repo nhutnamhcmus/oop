11edfa7472a67b3fa58bf543887726d61ab8852e
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 21:24:50 +0700

    Update Rectangle.h

--- W03/week-3/Rectangle.h
+++ W03/week-3/Rectangle.h
@@ -4,10 +4,29 @@
 
 #ifndef WEEK_3_RECTANGLE_H
 #define WEEK_3_RECTANGLE_H
-
+#include "Point.h"
 
 class Rectangle {
+private:
+    Point topLeft;
+    Point bottomRight;
 
+public:
+    Rectangle(const Point &topLeft, const Point &bottomRight);
+
+    Rectangle();
+
+    [[nodiscard]] const Point &getTopLeft() const;
+
+    void setTopLeft(const Point &topLeft);
+
+    [[nodiscard]] const Point &getBottomRight() const;
+
+    void setBottomRight(const Point &bottomRight);
+
+    [[nodiscard]] double area() const;
+
+    [[nodiscard]] double perimeter() const;
 };
 
 
