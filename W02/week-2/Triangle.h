1e52573362ef4c8a02009b2325451d4aba034ad8
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 13:35:37 +0700

    Update Triangle.h

--- W02/week-2/Triangle.h
+++ W02/week-2/Triangle.h
@@ -4,10 +4,27 @@
 
 #ifndef WEEK_2_TRIANGLE_H
 #define WEEK_2_TRIANGLE_H
-
+#include "Point.h"
 
 class Triangle {
+private:
+    Point node_a;
+    Point node_b;
+    Point node_c;
+public:
+    Triangle(const Point &nodeA, const Point &nodeB, const Point &nodeC);
 
+    [[nodiscard]] const Point &getNodeA() const;
+
+    void setNodeA(const Point &nodeA);
+
+    [[nodiscard]] const Point &getNodeB() const;
+
+    void setNodeB(const Point &nodeB);
+
+    [[nodiscard]] const Point &getNodeC() const;
+
+    void setNodeC(const Point &nodeC);
 };
 
 
