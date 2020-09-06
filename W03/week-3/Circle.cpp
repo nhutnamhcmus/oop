b78b7467bcc2bfe526024d7ac8ff797eefb239d9
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 14:04:47 +0700

    Update Circle.cpp

--- W03/week-3/Circle.cpp
+++ W03/week-3/Circle.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Circle.h"
+
+Circle::Circle(const Point &center, double radius) : center(center), radius(radius) {}
+
+const Point &Circle::getCenter() const {
+    return center;
+}
+
+void Circle::setCenter(const Point &center) {
+    Circle::center = center;
+}
+
+double Circle::getRadius() const {
+    return radius;
+}
+
+void Circle::setRadius(double radius) {
+    Circle::radius = radius;
+}
+
+double Circle::area() const {
+    return pow(Circle::getRadius(), 2) * M_PI;
+}
+
+double Circle::perimeter() const {
+    return 2 * Circle::getRadius() * M_PI;
+}
+
+double Circle::diameter() const {
+    return Circle::getRadius() * 2;
+}
