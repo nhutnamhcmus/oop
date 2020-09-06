1d310b73d72e8c909e66122bb19099819cd9e97e
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-06 17:30:24 +0700

    Update Student.h

--- W03/week-3/Student.h
+++ W03/week-3/Student.h
@@ -4,10 +4,36 @@
 
 #ifndef WEEK_3_STUDENT_H
 #define WEEK_3_STUDENT_H
-
+#include <string>
 
 class Student {
+private:
+    std::string firstName;
+    std::string middleName;
+    std::string lastName;
 
+public:
+    // constructors
+    Student(std::string firstName, std::string middleName, std::string lastName);
+
+    Student();
+
+    ~Student();
+
+    // getters and setters
+    [[nodiscard]] const std::string &getFirstName() const;
+
+    void setFirstName(const std::string &firstName);
+
+    [[nodiscard]] const std::string &getMiddleName() const;
+
+    void setMiddleName(const std::string &middleName);
+
+    [[nodiscard]] const std::string &getLastName() const;
+
+    void setLastName(const std::string &lastName);
+
+    [[nodiscard]] std::string fullName() const;
 };
 
 
