fde05f34f230988fffd9f44708475cfcc74efff3
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-13 17:11:38 +0700

    fix random

--- W04/week-4/Random.cpp
+++ W04/week-4/Random.cpp
@@ -3,17 +3,51 @@
 //
 
 #include "Random.h"
+#include <random>
+#include <chrono>
 
 Random::Random() = default;
 
 int Random::generate() {
-    return 0;
+    std::random_device rd;
+    std::mt19937::result_type seed = rd() ^ (
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now().time_since_epoch()
+                    ).count() +
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::microseconds>(
+                            std::chrono::high_resolution_clock::now().time_since_epoch()
+                    ).count() );
+
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<unsigned> distrib(1, 6);
+    return int(distrib(gen));
 }
 
+
+
 int Random::Next() {
-    return 0;
+    return this->generate();
 }
 
 int Random::Next(int max) {
-    return 0;
+    return Random::generate(0, max - 1);
+}
+
+int Random::generate(int a, int b) {
+    std::random_device rd;
+    std::mt19937::result_type seed = rd() ^ (
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::seconds>(
+                            std::chrono::system_clock::now().time_since_epoch()
+                    ).count() +
+            (std::mt19937::result_type)
+                    std::chrono::duration_cast<std::chrono::microseconds>(
+                            std::chrono::high_resolution_clock::now().time_since_epoch()
+                    ).count() );
+
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<unsigned> distrib(a, b);
+    return int(distrib(gen));
 }
