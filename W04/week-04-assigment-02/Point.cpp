af1839897a00e7d839cc3832d5c490ad472d0632
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-13 17:47:23 +0700

    Update Point.cpp

--- W04/week-04-assigment-02/Point.cpp
+++ W04/week-04-assigment-02/Point.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Point.h"
+
+Point::Point(const float &x, const float &y) : _x(x), _y(y) {}
+
+Point::Point() = default;
+
+Point::Point(const Point &another) {
+    *this = another;
+}
+
+Point::~Point() = default;
+
+Point &Point::operator=(const Point &another) {
+    this->setX(another.getX());
+    this->setY(another.getY());
+}
+
+float Point::getX() const {
+    return _x;
+}
+
+void Point::setX(float x) {
+    _x = x;
+}
+
+float Point::getY() const {
+    return _y;
+}
+
+void Point::setY(float y) {
+    _y = y;
+}
