2cbdd926037b98c3b03d38932267159d4fc78110
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-09-13 09:36:21 +0700

    point w04

--- W04/week-4/Point.cpp
+++ W04/week-4/Point.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Point.h"
+
+Point::Point(double x, double y) : x(x), y(y) {}
+
+Point::Point() : x(0), y(0) {}
+
+double Point::getX() const {
+    return x;
+}
+
+void Point::setX(double x) {
+    Point::x = x;
+}
+
+double Point::getY() const {
+    return y;
+}
+
+void Point::setY(double y) {
+    Point::y = y;
+}
+
+double Point::calculateDistanceTo(const Point &another) const {
+    return sqrt(pow(Point::getX() - another.getX(), 2) - pow(Point::getY() - another.getY(), 2));
+}
+
+double Point::calculateDistance(const Point &first_point, const Point &second_point) {
+    return first_point.calculateDistanceTo(second_point);
+}
+
+Point::~Point() = default;
--- W04/week-4/Point.h
+++ W04/week-4/Point.h
@@ -4,10 +4,35 @@
 
 #ifndef WEEK_4_POINT_H
 #define WEEK_4_POINT_H
-
+#include <cmath>
 
 class Point {
+private:
+    double x{};
+    double y{};
 
+public:
+    static int instanceCount;
+
+public:
+    // constructors
+    Point(double x, double y);
+
+    Point();
+
+    ~Point();
+    // getters and setters
+    [[nodiscard]] double getX() const;
+
+    void setX(double x);
+
+    [[nodiscard]] double getY() const;
+
+    void setY(double y);
+
+    [[nodiscard]] double calculateDistanceTo(const Point &another) const;
+
+    [[nodiscard]] static double calculateDistance(const Point&, const Point&);
 };
 
 
